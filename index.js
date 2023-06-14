641252a196828597148c040db0278ef7ce4b40de
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-14 01:26:30 -0600

    test using async fetch method

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let marr = null;
+  let books = fetch("https://anapioficeandfire.com/api/books")
+  .then(function(response)
+  {
+    marr = response.json()
+    return marr;
+  })
+  .then(function(marr){
+    console.log("1st marr = " + marr);
+    console.log("marr.length = " + marr.length);
+    //for (let n = 0; n < marr.length; n++)
+    //{
+      //console.log("keys = " + Object.keys(marr[n]));
+      //console.log("bookname = " + marr[n].name);
+    //}
+    renderBooks(marr);
+    return marr;
+  });
+  return books.then(books => books);
 }
 
 function renderBooks(books) {
